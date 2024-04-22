96819a30e692bb4ab26395f832a33d4c42ae112f
Yasha Ektefaie <yashaektefaie@yashas-mbp-3.harvardsecure.wireless.med.harvard.edu>
2024-04-22 10:09:21 -0400

    fixing setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='spectrae',
     version='0.1.0',    
     description='SPECTRA: The spectral framework for model evaluation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/mims-harvard/SPECTRA/',
     author='Yasha Ektefaie',
     author_email='yasha_ektefaie@g.harvard.edu',
